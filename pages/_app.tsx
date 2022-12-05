49f56f10dc61fedcddb0ac2013d77ffe5050e17a
Gabriel Mercante <gabriel.mercante@bolttech.io>
2022-12-05 06:40:08 -0300

    lint: fix camelcase issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
-import { Gothic_A1 } from '@next/font/google'
+import { Gothic_A1 as GothicA1 } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '../styles/global'
 import { defaultTheme } from '../styles/themes/default'
 
-const gothic = Gothic_A1({
+const gothic = GothicA1({
   weight: ['400', '700'],
   subsets: ['latin']
 })
